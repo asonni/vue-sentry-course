ca9c97640130b5ce6d15a5d4988b2e2cf582e56b
Aladdin Sonni <aladdin.sonni@gmail.com>
2022-07-31 15:36:34 +0200

    Add button to About page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,11 @@
+<script setup>
+const checkout = () => {
+  alert("Checking out items!");
+};
+</script>
 <template>
   <div>
     <h1 class="text-4xl">Home</h1>
     <button @click="checkout">Checkout</button>
   </div>
 </template>
-
-<script setup>
-const checkout = () => {
-  alert("Checking out items!");
-};
-</script>
